2184a81a0f6a3af612d43a157cd825402a2b4430
ThamizharasanT <96743694+ThamizharasanT@users.noreply.github.com>
2023-01-03 15:08:56 +0530

    Add files via upload

---
+++ tap to any place.swift
@@ -0,0 +1,9 @@
+  
+viewDidLoad(){
+
+let tap = UITapGestureRecognizer(target: self, action: #selector(dismisspopdelete(_:)))
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismisspopdelete(_:UITapGestureRecognizer){
+        popupDeleteBtn.isHidden = true
+    }
